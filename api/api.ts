20a61c42f18e89df41cb8cd72f74d8c680897672
HaileabT <haileabtesfaye11@gmail.com>
2025-01-27 07:51:40 -0500

    added cookie parser middleware

--- api/api.ts
+++ api/api.ts
@@ -1,10 +1,18 @@
 import express, { Express, json } from "express";
+import cookieParser from "cookie-parser";
 import { apiRouter } from "./routes/index.routes";
 import cors from "cors";
 const app: Express = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+app.use(cookieParser());
 app.use(json());
 
 // API Router
