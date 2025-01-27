81e39a6c6e31b6e36d15c8e2f05386fc13c5c8ac
HaileabT <haileabtesfaye11@gmail.com>
2025-01-27 07:49:52 -0500

    added logout route and protected some routes

--- api/routes/auth.routes.ts
+++ api/routes/auth.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { login } from "../controllers/auth.controllers";
+import { login, logout } from "../controllers/auth.controllers";
 import { catchControllerError } from "../utility/catchControllerErrors";
+import { AuthMiddleware } from "../middleware/AuthMiddleware";
 const authRouter = Router();
+const authMiddleware = AuthMiddleware.getMiddleware();
 
 authRouter.route("/login").post(catchControllerError(login));
+authRouter
+  .route("/logout")
+  .get(catchControllerError(authMiddleware.protectFromUnknownUser), catchControllerError(logout));
 
 export { authRouter };
--- api/routes/blog.routes.ts
+++ api/routes/blog.routes.ts
@@ -1,22 +1,18 @@
 import { Router } from "express";
 import { catchControllerError } from "../utility/catchControllerErrors";
-import {
-  createBlog,
-  findBlog,
-  findBlogById,
-  deleteBlog,
-  searchBlog,
-} from "../controllers/blog.controllers";
+import { createBlog, findBlog, findBlogById, deleteBlog, searchBlog } from "../controllers/blog.controllers";
 import { apiRouter } from "./index.routes";
+import { AuthMiddleware } from "../middleware/AuthMiddleware";
 
 const blogRouter = Router();
+const authMiddleware = AuthMiddleware.getMiddleware();
 
 blogRouter
   .route("/")
   .get(catchControllerError(findBlog))
-  .post(catchControllerError(createBlog));
+  .post(catchControllerError(authMiddleware.protectFromUnknownUser), catchControllerError(createBlog));
 blogRouter
   .route("/:id")
   .get(catchControllerError(findBlogById))
-  .delete(deleteBlog);
+  .delete(catchControllerError(authMiddleware.protectFromUnknownUser), catchControllerError(deleteBlog));
 export { blogRouter };
